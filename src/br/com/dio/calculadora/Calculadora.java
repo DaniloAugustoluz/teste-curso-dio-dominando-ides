23226c8a154f9b82b441c35996ea3c0727f3e5b6
Danilo Augusto Souza Luz <89174886+DaniloAugustoluz@users.noreply.github.com>
2021-09-10 20:53:15 -0300

    Primeiro Commit

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,39 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan =  new Scanner (System.in);
+		int a,b;
+		System.out.println("Digite um valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite outro valor: ");
+		b= scan.nextInt();
+		
+		int somar = soma(a,b);
+		int subtrair = subtracao(a,b);
+		float dividir = divisao(a,b);
+		int multiplicar = multiplicacao(a,b);
+		
+		System.out.println("Resultado da soma: " + somar);
+		System.out.println("Resultado da subtração: " + subtrair);
+		System.out.println("Resultado da divisão: " + dividir);
+		System.out.println("Resultado da multiplicação: " + multiplicar);
+	}
+	
+	public static int soma(int a,int b) {
+		return a + b;
+	}
+	public static int subtracao(int a,int b) {
+		return a - b;
+	}
+	public static float divisao(int a,int b) {
+		return a / b;
+	}
+	public static int multiplicacao(int a,int b) {
+		return a * b;
+	}
+	
+}
